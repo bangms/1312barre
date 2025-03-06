643271ad3551c0eec66e0e8c85573e4b02630b8b
SeoYeon <yeon2@4grit.com>
2025-03-07 00:21:43 +0900

    Create style files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Main from '../src/pages/Main';
+import About from '../src/pages/About';
+import Founder from '../src/pages/Founder';
+import Instructor from '../src/pages/Instructor';
+import Classes from '../src/pages/Classes';
+import FAQ from '../src/pages/FAQ';
+import Contact from '../src/pages/Contact';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="about" element={<About />} />
+          <Route path="founder" element={<Founder />} />
+          <Route path="instructor" element={<Instructor />} />
+          <Route path="classes" element={<Classes />} />
+          <Route path="faq" element={<FAQ/>} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

b2510abf91c8329a13b90bb47008e20fc0817586
SeoYeon <yeon2@4grit.com>
2025-03-10 23:40:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Main from '../src/pages/Main';
 import About from '../src/pages/About';
 import Founder from '../src/pages/Founder';
-import Instructor from '../src/pages/Instructor';
 import Classes from '../src/pages/Classes';
 import FAQ from '../src/pages/FAQ';
 import Contact from '../src/pages/Contact';
-import Header from './components/Header';
+import Layout from './components/Layout';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="about" element={<About />} />
-          <Route path="founder" element={<Founder />} />
-          <Route path="instructor" element={<Instructor />} />
-          <Route path="classes" element={<Classes />} />
-          <Route path="faq" element={<FAQ/>} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="about" element={<About />} />
+            <Route path="founder" element={<Founder />} />
+            <Route path="classes" element={<Classes />} />
+            <Route path="faq" element={<FAQ/>} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </div>
   );
